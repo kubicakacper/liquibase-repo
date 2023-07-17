94e170e355707f11548f0a1fb2e6d63d68abc535
Kacper Kubica <kubicakacper@gmail.com>
2023-07-17 16:48:59 +0200

    Update A_TABLE.sql

--- simple-project/changelogs/A_TABLE.sql
+++ simple-project/changelogs/A_TABLE.sql
@@ -1,7 +1,10 @@
 -- liquibase formatted sql
 
 -- changeset kacper:1
-CREATE TABLE A_TABLE (id INT, column2 VARCHAR(255), PRIMARY KEY (id))
+CREATE TABLE A_TABLE (id INT, a_column VARCHAR(255), PRIMARY KEY (id));
+-- rollback DROP TABLE A_TABLE;
 
 -- changeset kacper:2
-ALTER TABLE A_TABLE ADD COLUMN column3 VARCHAR(255)
+ALTER TABLE A_TABLE ADD COLUMN new_column VARCHAR(255);
+CREATE INDEX IX ON A_TABLE (new_column);
+-- rollback ALTER TABLE A_TABLE DROP COLUMN column3; DROP INDEX IX;
